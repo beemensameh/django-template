1f5795cf6c81c58eaa13fe7c3ff1da17e5aaf14d
Beemen Sameh <41084077+BeemenSameh@users.noreply.github.com>
2021-06-12 19:09:38 +0000

    initialize django template

---
+++ README.md
@@ -0,0 +1,34 @@
+# Microservice Template
+
+**Swagger UI:** link here .. 
+
+## Developer
+
+### Prepare the Repo
+- Fork this repo to the new group created for the project.
+- `cd` to the project directory
+- Create an environment and install requirements for the local development: `poetry install`
+- Activate the environment: `poetry shell`
+
+Now, your project is set up and you can go ahead to create apps
+
+### Start development
+- Go to source folder `cd src`
+- Create a new app `./manage.py startapp <appname>`
+- Edit the settings file to include your app. `INSTALLED_APPS = ('cool-app', )`. Don't forget the trailing `,` to make it a tuple.
+- Add your `url` files to `config/urls.py`
+- Run project: `./manage.py runserver`
+
+## Deployment Instructions
+
+- In `docker-compose.yml`, replace the port number 8000 to the server deployment port number. 
+- Rename `gitlab-ci-example.yml` to `gitlab-ci.yml`
+- In `gitlab-ci.yml`. Replace every `microservice-template` in any key you see to the microservice name.
+- SSH to the server and create a deployment environment file at `/xware/deployments/microservice-template/.env src/.env`
+- Edit the keys in `.env`. Make sure you provide keys for the database so that the default SQLite database is ignored
+- Register the GitLab runner: `sudo gitlab-runner register`
+- `git push` on the master branch.
+
+## Governance
+As deployment master, you should consider the following:
+- In order to have a separate *production* and *deployment* servers, consider using a branch called `staging`. In the `gitlab-ci.yml` file, add `staging` as a tag value to `tags` key. When registering the runner on the staging server, include `staging` in the tags key. Then, pushes on this branch will automatically deploy to the staging server, while pushes to the master branch will deploy to the production server.
---
+++ src/config/urls.py
@@ -0,0 +1,66 @@
+"""{{project_name}} URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework import permissions
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title='{{project_name}} API',
+            default_version='0.1.0',
+            description='{{project_name}} API',
+            terms_of_service='https://example.com/',
+            contact=openapi.Contact(email='beeaccdeveloper@gmail.com'),
+            license=openapi.License(
+                name='BSD License',
+                url='https://example.com/'
+            )
+        ),
+        public=True,
+        permission_classes=(permissions.IsAuthenticated,),
+    )
+
+    urlpatterns += [
+        path('admin/doc/', include('django.contrib.admindocs.urls')),
+        url(
+            r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'
+        ),
+        url(
+            r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'
+        ),
+        url(
+            r'^redoc/$',
+            schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'
+        ),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
