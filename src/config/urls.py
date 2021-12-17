3eefe05147c8dea7338322c62987a54dc55a3ee2
Beemen Sameh <41084077+BeemenSameh@users.noreply.github.com>
2021-12-17 11:59:12 +0000

    upgrate to django 4.0

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,7 @@
 """{{project_name}} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 
+import debug_toolbar
 from rest_framework import permissions
 
 urlpatterns = [
@@ -47,20 +47,21 @@ if settings.DEBUG:
 
     urlpatterns += [
         path('admin/doc/', include('django.contrib.admindocs.urls')),
-        url(
+        re_path(
             r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0),
             name='schema-json'
         ),
-        url(
+        re_path(
             r'^swagger/$',
             schema_view.with_ui('swagger', cache_timeout=0),
             name='schema-swagger-ui'
         ),
-        url(
+        re_path(
             r'^redoc/$',
             schema_view.with_ui('redoc', cache_timeout=0),
             name='schema-redoc'
         ),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
         + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
