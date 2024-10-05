4c08ca0d6fb539c1a67bd2ec0a0b47cdf50a25bf
beemensameh <41084077+beemensameh@users.noreply.github.com>
2024-10-05 22:21:37 +0000

    bugfix: fix url pattern issue

--- src/config/settings.py
+++ src/config/settings.py
@@ -211,7 +211,9 @@ if DEBUG:
         "VERSION": "0.1.0",
         "SCHEMA_PATH_PREFIX": "/api/v[0-9]",
         "SERVE_INCLUDE_SCHEMA": False,
-        "SERVE_AUTHENTICATION": ["rest_framework.authentication.SessionAuthentication"],
+        "SERVE_AUTHENTICATION": [
+            "rest_framework.authentication.SessionAuthentication"
+        ],
         "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
         "SWAGGER_UI_SETTINGS": {
             "deepLinking": True,
--- src/config/urls.py
+++ src/config/urls.py
@@ -36,21 +36,20 @@ if settings.DEBUG:
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-    urlpatterns.insert(
-        0,
-        [
-            path("admin/schema/", SpectacularAPIView.as_view(), name="schema"),
-            path(
-                "admin/swagger/",
-                SpectacularSwaggerView.as_view(url_name="schema"),
-                name="swagger-ui",
-            ),
-            path(
-                "admin/redoc/",
-                SpectacularRedocView.as_view(url_name="schema"),
-                name="redoc",
-            ),
-            path("admin/doc/", include("django.contrib.admindocs.urls")),
-            path("__debug__/", include(debug_toolbar.urls)),
-        ],
-    )
+    urlpatterns_debug = [
+        path("admin/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "admin/swagger/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "admin/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+        path("admin/doc/", include("django.contrib.admindocs.urls")),
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
+
+    urlpatterns = [*urlpatterns, *urlpatterns_debug]
